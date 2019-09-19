4c3c84acb7b3e5492f160050a919d83e579fe20e
jason <jasonedlewis@gmail.com>
2019-09-18 20:35:30 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,55 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
+  handleLoginFeilds = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  onSubmit = event => {
+    
+    event.preventDefault();
+    !this.isValid() ? (this.props.handleLogin(this.state)) : 0
+    
+  };
+
+  isValid=()=>{
+    return (this.state.username.length === 0 || this.state.password.length === 0)
+  }
   render() {
+    console.log(this.state);
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleLoginFeilds}
+              // disabled={this.state.username.length === 0}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleLoginFeilds}
+              // disabled={this.state.password.length < 1}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,30 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
   }
+
+  messageHandler = (e) => {    
+    this.setState({
+      message: e.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={this.messageHandler}
+          disabled={this.state.message.length >= this.props.maxChars}
+        />
+        <p>Remaining Characters: {this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
