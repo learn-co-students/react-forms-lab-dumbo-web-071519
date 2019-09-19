57447381a1aae3bc58ceff1cbb149cabd366d67e
Gulnoza Muminova <gmcharmy@gmail.com>
2019-09-18 20:30:56 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,47 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onSubmit=(event) => {
+  event.preventDefault()
+    if (this.state.username.length !== 0 && this.state.password.length !== 0){
+      this.props.handleLogin(this.state)
+    }
+    
+    
+
+  }
+  onChange=(event) => {
+    // event.preventDefault()
+   // console.log(event)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.onChange} id="username"  name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.onChange} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" disabled={this.state.username.length === 0 || this.state.password.length === 0 }>Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,37 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    
+    };
+   
+}
+  
+ onChange=(event)=>{
+  
+  //  event.preventDefault()
+
+   this.setState({
+     message: event.target.value
+
+   })
+   console.log(this.state.message)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.onChange} 
+        value={this.state.message} 
+        type="text"
+         name="message"
+          id="message" 
+          disabled = {this.state.message.length >= this.props.maxChars}/>
+        <p>Remaining characters counter: {this.props.maxChars - this.state.message.length  }</p>
       </div>
+
     );
   }
 }
